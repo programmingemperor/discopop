e5ad3bccd7f21e37e022e37c9475ec6e571dfdc8
Raynard99 <raynard.widjaja@gmail.com>
2021-11-08 22:49:51 +0000

    testing discopop

--- run-normal.sh
+++ run-normal.sh
@@ -1,16 +1,20 @@
 /home/raynardwidjaja/discopop/scripts/dp-fmap
 
 #CU Generation
-clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynardwidjaja/discopop/build/libi/LLVMCUGeneration.so -mllvm -fm-path -mllvm ./FileMapping.txt -c test.c
+clang-8 -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynardwidjaja/discopop/build/libi/LLVMCUGeneration.so -mllvm -fm-path -mllvm ./FileMapping.txt -c test.c
 
 #Dependence Profiling
-clang -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynardwidjaja/discopop/build/libi/LLVMDPInstrumentation.so -mllvm -fm-path -mllvm ./FileMapping.txt -c test.c -o test.o
-clang++ test.o -L /home/raynardwidjaja/discopop/build/rtlib -lDiscoPoP_RT -lpthread
+clang-8 -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynardwidjaja/discopop/build/libi/LLVMDPInstrumentation.so -mllvm -fm-path -mllvm ./FileMapping.txt -c test.c -o test.o
+clang++-8 test.o -L /home/raynardwidjaja/discopop/build/rtlib -lDiscoPoP_RT -lpthread
 
+mv a.out test
 ./test
 
 #Reduction
-clang++ -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynardwidjaja/discopop/build/libi/LLVMDPReduction.so -mllvm -fm-path -mllvm ./FileMapping.txt -c test.c -o out.o
-clang++ test.o -L /home/raynardwidjaja/discopop/build/rtlib -lDiscoPoP_RT -lpthread
+clang++-8 -g -O0 -fno-discard-value-names -Xclang -load -Xclang /home/raynardwidjaja/discopop/build/libi/LLVMDPReduction.so -mllvm -fm-path -mllvm ./FileMapping.txt -c test.c -o out.o
+clang++-8 test.o -L /home/raynardwidjaja/discopop/build/rtlib -lDiscoPoP_RT -lpthread
 
-./test
+mv a.out test
+./test 
+
+mv test_dep.txt dp_run_dep.txt
