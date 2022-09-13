75f28d8d36a1a07aad83d3d3e41391c1b1f980e4
[programmingemperor] <[kobran.jeterson@gmail.com]>
2022-09-13 14:36:29 +0200

    state with small gain of 1.14

--- test/rust/performance/main.rs
+++ test/rust/performance/main.rs
@@ -2,10 +2,12 @@ use rayon::prelude::*;
 use std::time::{Duration, Instant}; 
 use std::thread::sleep;
 
-const MAX_N: usize = 100;
+const iterations: usize = 100000; 
 
 fn main() {
     // We can use this function here, and define it somewhere later
+
+  
 
     println!("SEQUENTIAL REDUCTION"); 
     reduction(); 
@@ -35,19 +37,19 @@ fn expensiveOP(n: i32) -> i32 {
     }
 }
 
+
+
 fn reduction() -> () {
 
 
-    let iterations: i32 = 10000; 
-    let localArr: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    let arr1: [i32; iterations] = [0; iterations];
+    let arr2: [i32; iterations] = [0; iterations];
+
 
     let (mut a, mut b, mut c): (i32, i32, i32) = (0, 0, 0); 
 
     let mut now = Instant::now();
-    //let dur: Duration = Duration::new(2, 0); 
-
-    //sleep(dur);
-    //println!("{}", "sleeping"); 
+ 
 
     for i in 0..iterations {
         a = a + 1; 
@@ -59,18 +61,17 @@ fn reduction() -> () {
 
 
 
-   
 
 
     let mut localRes: i32 = 0; 
     now = Instant::now();
 
-    for i in 0..localArr.len() {
-        b = b * 2; 
+    for i in 0..arr1.len() {
+        b = b + 2; 
         c = c - (i as i32); 
         let tmp = a; 
         a = tmp + (i as i32); 
-        localRes = localArr[i] * 3 + localRes; 
+        localRes = arr1[i] + 3 + localRes; 
     }
     println!("Sequential reduction example 2:{}", now.elapsed().as_nanos());
 
@@ -81,9 +82,9 @@ fn reduction() -> () {
 
     now = Instant::now();
 
-    for i in 0..localArr.len() {
-        arrSum = arrSum + localArr[i]; 
-        arrMul = arrMul + localArr[i]; 
+    for i in 0..arr1.len() {
+        arrSum = arrSum + arr1[i]; 
+        arrMul = arrMul + arr1[i]; 
     }
     println!("Sequential reduction example 3: {}", now.elapsed().as_nanos());
 
@@ -108,16 +109,14 @@ fn reduction() -> () {
 
 fn do_all() -> () {
 
-    //let mut localarr: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    //let mut arr: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    let mut localarr: [i32; 20000] = [0; 20000]; 
-    let mut arr: [i32; 20000] = [0; 20000];
+    let mut arr1: [i32; iterations] = [0; iterations];
+    let mut arr2: [i32; iterations] = [0; iterations];
 
 
     let mut now = Instant::now();
 
-    for i in 0..arr.len() {
-        arr[i] = 0; 
+    for i in 0..arr1.len() {
+        arr1[i] = 0; 
      }
      println!("Sequential doall example 1: {}",  now.elapsed().as_nanos()); 
   
@@ -125,44 +124,29 @@ fn do_all() -> () {
     
     now = Instant::now();
 
-    for i in 0..arr.len() {
+    for i in 0..arr1.len() {
 
-        let tmp = localarr[i]; 
-        localarr[i] = arr[i];  
-        arr[i] = tmp; 
+        let tmp = arr2[i]; 
+        arr2[i] = arr1[i];  
+        arr1[i] = tmp; 
  
      } 
-    println!("Sequential doall example 2: {}", now.elapsed().as_nanos()/1000); 
+    println!("Sequential doall example 2: {}", now.elapsed().as_nanos()); 
     
     }    
 
 
 fn parallel_reduction() -> () {
 
-    /*let a: i32 = 0..iterations.par_iter() // <-- just change that!
-    .map(|&i| 1 )
-    .sum(); 
-
-    println!("sum = {}", a); */
-    // how to map this best to parallel iterators?
-
-    let iterations: i32 = 10000; 
-    let localArr: [i32; MAX_N] = [0; MAX_N];
+     
+    let mut arr1: [i32; iterations] = [0; iterations];
+    let mut arr2: [i32; iterations] = [0; iterations];
 
     let (mut a, mut b, mut c): (i32, i128, i32) = (0, 0, 0); 
 
-    
-    /*let sums = [(0, 1), (5, 6), (16, 2), (8, 9)]
-           .par_iter()        // iterating over &(i32, i32)
-           .cloned()          // iterating over (i32, i32)
-           .reduce(|| (0, 0), // the "identity" is 0 in both columns
-                   |a, b| (a.0 + b.0, a.1 + b.1));*/
 
-          
 
     let mut now = Instant::now();
-
-
 
     let result = (0..iterations)
             .into_par_iter()
@@ -178,7 +162,7 @@ fn parallel_reduction() -> () {
 
   
 
-    // without array for this one due to limited closure access
+
     now = Instant::now();
 
     let result = (0..iterations)
@@ -191,23 +175,17 @@ fn parallel_reduction() -> () {
 
     println!("Parallel reduction example 2 {}", now.elapsed().as_nanos());         
         
-    // let r = (0..4)
-    // .into_par_iter()
-    // .map(|_| 1)
-    // .for_each(|i| {
-    //     println!("printing a in iteration: {}", i); 
-    // });
-    // // .reduce(|| a, 
-    // //     |a1, a2| a1 + a2); 
+ 
+    
 
     let mut arrSum: i32 = 0; 
     let mut arrMul: i32 = 1; 
 
     now = Instant::now();
 
-    let arraySum = (0..localArr.len())
+    let reduction = (0..arr1.len())
     .into_par_iter()
-    .map(|i| (localArr[i], localArr[i]))
+    .map(|i| (arr1[i], arr2[i]))
     .reduce(
         || (arrSum, arrMul), 
         |(a1,  c1), (a2, c2)| (a1 + a2,  c1 * c2),
@@ -219,9 +197,8 @@ fn parallel_reduction() -> () {
 
 
 
-    now = Instant::now();
 
-    //function call
+    now = Instant::now();
 
     (0..20)
     .into_par_iter()
@@ -241,41 +218,28 @@ fn parallel_reduction() -> () {
 
 fn parallel_doall() -> () {
 
-    let mut localarr: [i32; 20000] = [1; 20000]; 
-    let mut arr: [i32; 20000] = [7; 20000];
+    let mut arr1: [i32; iterations] = [0; iterations];
+
+    let mut arr2: [i32; iterations] = [0; iterations]; 
+
 
     let mut now = Instant::now();
 
-
-
-    arr.par_iter_mut()
+    arr1.par_iter_mut()
     .for_each(|x| {
-        *x = 8;
+        *x = 1;
     });
-
-    //(0..5).into_par_iter().map(|x| x * 2);
 
 
     println!("Parallel doall example 1: {}", now.elapsed().as_nanos());
 
 
-
-
-/*
-    (0..localarr.len())
-    .into_par_iter()
-    .for_each(|index| {
-        let tmp = localarr[index]; 
-        localarr[index] = arr[index];  
-        arr[index] = tmp;
-    }); */
-
     
     now = Instant::now();
 
-    localarr
+    arr1
     .par_iter_mut()
-    .zip(arr.par_iter_mut())
+    .zip(arr2.par_iter_mut())
     .for_each(|tuple| {
         // store *tuple1 in temp and do the same swap
         let tmp = *tuple.0; 
@@ -284,23 +248,9 @@ fn parallel_doall() -> () {
 
     });
 
-    println!("Parallel doall example 2: {}", now.elapsed().as_nanos()/1000);
+    println!("Parallel doall example 2: {}", now.elapsed().as_nanos());
     
 
-
-
-    
-
-    //maybe when labeled as unsafe?
-    /*(0..arr.len()).into_par_iter()
-    .for_each(|i| {
-        let mut tmp = localarr[i]; 
-        localarr[i] = arr[i];
-        arr[i] = tmp; 
-     }
-    );*/
-
-    println!("Parallel doall example 2: {}", now.elapsed().as_nanos()); 
 
 
 
@@ -308,4 +258,3 @@ fn parallel_doall() -> () {
 
 
 }
-
