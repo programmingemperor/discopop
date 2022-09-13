2fd5bfa4d10b49dc7ad15f07fa7236bbb92c59a3
[programmingemperor] <[kobran.jeterson@gmail.com]>
2022-09-13 13:13:01 +0200

    added performance output over number of iterations

--- test/swift_test/performance/parallelism_examples.swift
+++ test/swift_test/performance/parallelism_examples.swift
@@ -7,8 +7,8 @@ var cG = 0;
 var localRes = 0; 
 var arrSum = 0, arrMul = 0; 
 
-//same thing
-var N = 25; 
+
+var N = 1000; 
 
 
 func getArr(iterations: Int) -> Array<Int> {
@@ -21,7 +21,7 @@ func getArr(iterations: Int) -> Array<Int> {
 
 var globalArr = getArr(iterations: iterations); 
 
-//TODO: finish dispatch reduction examples, find 3 more examples for each, run for time measurments and correctness
+
 
 
 func expensiveOP(n: Int) -> Int {
@@ -73,9 +73,9 @@ func reduction() {
     start = DispatchTime.now() 
     for i in 0...iterations {
       
-        bG = bG * 5; 
+        bG = bG + 5; 
         c = c - i; 
-        localRes = localArr[i] * 3 + localRes; 
+        localRes = localArr[i] + 3 + localRes; 
 
         let tmp = a;
         a = tmp + i;
@@ -197,7 +197,8 @@ func reduction_concurrent() {
      
     var stride = N; 
     // too much for this task
-    var number_of_tasks = globalArr.count/stride;
+    var number_of_tasks = iterations/stride;
+    print("number of tasks", number_of_tasks)
 
     // residue
 
@@ -224,8 +225,9 @@ func reduction_concurrent() {
         // start index of current array is tasknumber* offset 
         // stride(length of each task)
         var stride = N; 
-        var j = index * stride+1;
+        var j = index * stride;
         var stride_end = j + stride;
+        print("task nr", index); 
 
         var privA = 0, privB = 0, privC = 0; 
 
@@ -238,9 +240,9 @@ func reduction_concurrent() {
         } while (j < stride_end);
 
         queue.sync{ 
-        a = privA; 
-        b = privB; 
-        c = privC; 
+        a = a + privA; 
+        b = b + privB; 
+        c = c + privC; 
     }
     }
 
@@ -267,23 +269,24 @@ func reduction_concurrent() {
     .concurrentPerform(iterations: number_of_tasks){ (index) in
 
         var stride = N; 
-        var j = index * stride+1;
+        var j = index * stride;
         var stride_end = j + stride;
 
         var privA = 0, privB = 1, privC = 0, localResPriv = 0; 
 
         repeat {
         privA = privA + j; 
-        privB = privB * 5; 
+        privB = privB + 5; 
         privC = privC - j; 
-        localResPriv = localArr[j] * 3 + localResPriv;
+        localResPriv = localArr[j] + 3 + localResPriv;
 
         j = j + 1; 
         } while (j < stride_end);
+        
 
         queue.sync{ 
         a = a + privA; 
-        bG = bG * privB; 
+        bG = bG + privB; 
         c = c - privC; 
         localRes = localRes + localResPriv; 
     }
@@ -318,7 +321,7 @@ func reduction_concurrent() {
         // start index of current array is tasknumber* offset 
         // stride(length of each task)
         var stride = N; 
-        var j = index * stride+1;
+        var j = index * stride;
         var stride_end = j + stride;
 
         var privarrSum = 0, privarrMul = 1; 
@@ -375,7 +378,7 @@ func reduction_concurrent() {
         // start index of current array is tasknumber* offset 
         // stride(length of each task)
         //var stride = N; 
-        var j = index * stride+1;
+        var j = index * stride;
         var stride_end = j + stride;
 
         var privA = 0;
@@ -394,7 +397,7 @@ func reduction_concurrent() {
         queue.sync{
         
         a = a + privA; 
-        cG = cG * privcG; 
+        cG = cG + privcG; 
 
         } 
 
@@ -412,6 +415,7 @@ func reduction_concurrent() {
     timeInterval = Double(nanoTime) / 1_000_000_000
     print("dispatch reduction example 4: ", timeInterval)
 
+
 }
 
 
@@ -428,7 +432,7 @@ func doall_concurrent() {
     var localArr = getArr(iterations: iterations); 
 
     var stride = N; 
-    var number_of_tasks = globalArr.count/stride; 
+    var number_of_tasks = iterations/stride; 
     // residue for uneven arrays
     
     var queue = DispatchQueue(label: "reductionQueue", attributes: .concurrent);
@@ -444,7 +448,7 @@ func doall_concurrent() {
     .concurrentPerform(iterations: number_of_tasks) { (index) in
 
         // just stride
-        var j = index * stride+1;
+        var j = index * stride;
         var stride_end = j + stride;
       
         repeat {
@@ -478,7 +482,7 @@ func doall_concurrent() {
     .concurrentPerform(iterations: number_of_tasks) { (index) in
 
         // just stride
-        var j = index * stride+1;
+        var j = index * stride;
         var stride_end = j + stride;
       
         repeat {
@@ -530,3 +534,138 @@ func entry() {
 
 
 entry(); 
+
+
+/*
+
+stride 10000: 
+
+SEQUENTIAL REDUCTION
+sequential reduction example 1:  0.000208848
+sequential reduction example 2:  0.001449642
+sequential reduction example 3:  0.000372157
+sequential reduction example 4:  1.081587362
+
+
+SEQUENTIAL DOALL
+sequential doall example 1:  0.000213156
+sequential doall example 2:  0.000384366
+
+
+CONCURRENT REDUCTION 
+dispatch reduction example 1:  5.7751e-05 = 0.00005
+dispatch reduction example 2:  6.6211e-05
+dispatch reduction example 4:  0.274854242
+
+
+CONCURRENT DOALL
+dispatch doall example 1:  0.000294819
+dispatch example 2:  0.000792413
+
+
+
+stride 1000: 
+
+SEQUENTIAL REDUCTION
+sequential reduction example 1:  0.000120911
+sequential reduction example 2:  0.000824055
+sequential reduction example 3:  0.00031972
+sequential reduction example 4:  1.068835035
+
+
+SEQUENTIAL DOALL
+sequential doall example 1:  0.000227915
+sequential doall example 2:  0.000389918
+
+
+CONCURRENT REDUCTION 
+dispatch reduction example 1:  0.000627018
+dispatch reduction example 2:  0.000297453
+dispatch reduction example 4:  0.261780059
+
+
+CONCURRENT DOALL
+dispatch doall example 1:  0.000129481
+dispatch example 2:  0.000263437
+
+
+
+stride 25: 
+
+SEQUENTIAL REDUCTION
+sequential reduction example 1:  0.000249884
+sequential reduction example 2:  0.001549177
+sequential reduction example 3:  0.000233431
+sequential reduction example 4:  1.157672886
+
+
+SEQUENTIAL DOALL
+sequential doall example 1:  0.000231674
+sequential doall example 2:  0.000370542
+
+
+CONCURRENT REDUCTION 
+dispatch reduction example 1:  0.030082164
+dispatch reduction example 2:  0.00098404
+dispatch reduction example 4:  0.27025934
+
+
+CONCURRENT DOALL
+dispatch doall example 1:  0.000173879
+dispatch example 2:  0.000205735
+examples done
+
+
+stride 5: 
+
+
+SEQUENTIAL REDUCTION
+sequential reduction example 1:  0.000143518
+sequential reduction example 2:  0.000977895
+sequential reduction example 3:  0.000375864
+sequential reduction example 4:  1.106987608
+
+
+SEQUENTIAL DOALL
+sequential doall example 1:  0.000260302
+sequential doall example 2:  0.000410097
+
+
+CONCURRENT REDUCTION 
+dispatch reduction example 1:  0.031031167
+dispatch reduction example 2:  0.001739485
+dispatch reduction example 4:  0.268966902
+
+
+CONCURRENT DOALL
+dispatch doall example 1:  0.000325007
+dispatch example 2:  0.000321672
+
+
+stride 1: 
+
+SEQUENTIAL REDUCTION
+sequential reduction example 1:  8.8426e-05 = 0.00008
+sequential reduction example 2:  0.000594563
+sequential reduction example 3:  0.000246565
+sequential reduction example 4:  1.050489971
+
+
+SEQUENTIAL DOALL
+sequential doall example 1:  0.000286561
+sequential doall example 2:  0.000494953
+
+
+CONCURRENT REDUCTION 
+dispatch reduction example 1:  0.006035617
+dispatch reduction example 2:  0.006202578
+dispatch reduction example 4:  0.255792142
+
+
+CONCURRENT DOALL
+dispatch doall example 1:  0.001032457
+dispatch example 2:  0.001134735
+
+
+
+*/
